3c806e6326b3d2c8b73ba0932df95349350208a7
SamreenK <kausarsamreen28@gmail.com>
2022-05-21 20:51:52 +0400

    Added funtion to decide winner

--- js/game.js
+++ js/game.js
@@ -12,11 +12,42 @@ function playerSelection(){
     const playerValue = prompt("Please Select option[Rock/Paper/Scissor");
     return playerValue;
 }
+//winner Selection
  function Gameplay(){
-     const playerSelect = playerSelection().toUpperCase();
-     const compSelect = computerSelection().toUpperCase();
+     const playerSelect = playerSelection().toLowerCase();
+     const compSelect = computerSelection().toLowerCase();
      console.log( "Player selected :",playerSelect);
      console.log("Computer Selected :", compSelect);
+     if(playerSelect===compSelect){
+        console.log("Its a tie");
+     }
+     else if(playerSelect==='rock'){
+            if(compSelect==='paper')
+            console.log("You lose! Paper beaats Rock");
+            else if(compSelect==='scissor')
+            console.log('You WIn! Rock beats Scissor');
+        }
+    else if(playerSelect==='paper'){
+        if(compSelect==='rock')
+        console.log('You Win ! Paper beaats Rock');
+        else if(compSelect==='scissor')
+        console.log("You lose! Scissor beaats paper");
+        }
+    else if(playerSelect==='scissor'){
+        if(compSelect==='paper')
+        console.log('You win! Scissor beats Paper');
+        else if(compSelect==='rock')
+        console.log('You lose! Rock beats scissor');
+    } 
+ }
+ // rounds to be played
+ function playRound(){
+     const rounds = 5;
+     const compScore=0;
+     const playerScore=0;
+     console.log('Best of five game');
+     for(i=0; i<rounds; i++){
+        Gameplay();
+     }
 
-     
  }
