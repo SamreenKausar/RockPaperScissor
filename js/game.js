0b46f5392c943155374f2a370725a8591361f87a
SamreenK <kausarsamreen28@gmail.com>
2022-05-21 17:05:44 +0400

    Added  3 funtions two for selection and other result

--- js/game.js
+++ js/game.js
@@ -2,7 +2,21 @@
 // global variables
 const values = ['Rock', 'Paper', 'Scissor'];
 //function generating ROCK/PAPER/SCISSOR randomly
-function computerPlay (){
+function computerSelection (){
     const random = Math.floor(Math.random()*3);
-    console.log(values[random]);
+    const compValue = values[random];
+    return compValue ;
 }
+// function selecting players input
+function playerSelection(){
+    const playerValue = prompt("Please Select option[Rock/Paper/Scissor");
+    return playerValue;
+}
+ function Gameplay(){
+     const playerSelect = playerSelection().toUpperCase();
+     const compSelect = computerSelection().toUpperCase();
+     console.log( "Player selected :",playerSelect);
+     console.log("Computer Selected :", compSelect);
+
+     
+ }
