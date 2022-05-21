0f17a1037737c124a765d97724eb6314a17faa28
SamreenK <kausarsamreen28@gmail.com>
2022-05-21 16:43:17 +0400

    Made values global variable

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 // console.log("hello World");
+// global variables
+const values = ['Rock', 'Paper', 'Scissor'];
 //function generating ROCK/PAPER/SCISSOR randomly
 function computerPlay (){
-    const values = ['Rock', 'Paper', 'Scissor'];
     const random = Math.floor(Math.random()*3);
     console.log(values[random]);
 }
