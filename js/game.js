01a82732f35d31346ae28836492a06bac8fcad0a
SamreenK <kausarsamreen28@gmail.com>
2022-05-21 16:41:04 +0400

    Added computer play function

--- js/game.js
+++ js/game.js
@@ -1,1 +1,7 @@
-console.log("hello World");
+// console.log("hello World");
+//function generating ROCK/PAPER/SCISSOR randomly
+function computerPlay (){
+    const values = ['Rock', 'Paper', 'Scissor'];
+    const random = Math.floor(Math.random()*3);
+    console.log(values[random]);
+}
