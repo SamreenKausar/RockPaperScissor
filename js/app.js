2f579dbbd9704456127576eed7ad448caf5f0355
SamreenK <kausarsamreen28@gmail.com>
2022-05-31 12:53:50 +0400

    Refactored according to dom tree

--- js/app.js
+++ js/app.js
@@ -1,70 +1,86 @@
 // global variables
 const values = ['Rock', 'Paper', 'Scissor'];
+const buttonSelect = document.querySelectorAll('.buttons');
+const playerScoreId = document.querySelector('#playerScoreId');
+const compScoreId = document.querySelector('#compScoreId');
+const compSelectId = document.querySelector('#compSelect');
+const playerSelectId = document.querySelector('#playerSelect');
+const reset = document.querySelector('#reset');
+
+
+
+
 //function generating ROCK/PAPER/SCISSOR randomly for computer
+
 function computerSelection (){
     const random = Math.floor(Math.random()*3);
     const compValue = values[random].toLowerCase();
     return compValue ;
 }
-// function selecting players input
-function playerSelection(){
-    let playerValue = prompt("Please Select option[Rock/Paper/Scissor]").toLowerCase();
-    if(playerValue==='rock'|| playerValue === 'paper'|| playerValue === 'scissor')
-    return playerValue;
-    else {
-    playerValue = prompt('Please enter valid value').toLowerCase();
-    return playerValue;
-    }
-}
-//winner Selection
- function Gameplay(){
-     const playerSelect = playerSelection();
-     const compSelect = computerSelection();
-     console.log( "Player selected :",playerSelect);
-     console.log("Computer Selected :", compSelect);
-     if(playerSelect===compSelect){
-        return 'both';
-     }
+ // function for round play
+
+ function playRound(e){
+    const playerSelect = e.target.getAttribute('id');
+    const compSelect = computerSelection();
+    compSelectId.setAttribute('src',`images/${compSelect}.png`);
+    playerSelectId.setAttribute('src',`images/${playerSelect}.png`);
+    // determining winner of the round
+    if(playerSelect===compSelect){
+        compScore++;
+        compScoreId.textContent = compScore;
+        playerScore++;
+        playerScoreId.textContent = playerScore;
+     } 
      else if(playerSelect==='rock'){
-            if(compSelect==='paper')
-            return 'comp';
-            else if(compSelect==='scissor')
-            return 'player';
+        if(compSelect==='paper'){
+            compScore++;
+            compScoreId.textContent = compScore;
         }
+        else if(compSelect==='scissor'){
+            playerScore++;
+            playerScoreId.textContent = playerScore;
+        }
+    } 
     else if(playerSelect==='paper'){
-        if(compSelect==='rock')
-        return 'player';
-        else if(compSelect==='scissor')
-        return 'comp';
+        if(compSelect==='rock'){
+            playerScore++;
+            playerScoreId.textContent = playerScore;
+        }
+        else if(compSelect==='scissor'){
+            compScore++;
+            compScoreId.textContent = compScore; 
+        }
         }
     else if(playerSelect==='scissor'){
-        if(compSelect==='paper')
-        return 'player';
-        else if(compSelect==='rock')
-        return 'comp';
+        if(compSelect==='paper'){
+            playerScore++;
+            playerScoreId.textContent = playerScore;
+
+        }
+        else if(compSelect==='rock'){
+            compScore++;
+            compScoreId.textContent = compScore;
+        }
     } 
  }
- // the game played by default 5 rounds 
- function playRound(){
-     const rounds = 5;
-     let compScore = 0;
-     let playerScore =0;
-     console.log('Best of five game');
-     for(i=0; i<rounds; i++){
-        let result;
-        result = Gameplay();
-        if(result==='both'){
-        compScore++;
-        playerScore++;
-        console.log('the winner:',result);
-       } else if(result === 'comp'){
-        compScore++;
-        console.log('the winner:',result);
-       } else if(result === 'player'){
-        playerScore++;
-        console.log('the winner:',result);
-       }
-     }
-     console.log(`the final score computer:${compScore} and player: ${playerScore}` )
- }
- playRound();
+// Function for Reseting Game
+
+function resetGame(){
+    compScore = 0;
+    playerScore = 0;
+    compScoreId.textContent = compScore;
+    playerScoreId.textContent = playerScore;
+    compSelectId.setAttribute('src',`images/rock.png`);
+    playerSelectId.setAttribute('src',`images/rock.png`); 
+   }
+
+// Game Play
+
+resetGame();
+buttonSelect.forEach((btn)=>{
+    btn.addEventListener('click', playRound);
+ });
+// reset game
+reset.addEventListener('click', ()=>{
+    resetGame();
+});
